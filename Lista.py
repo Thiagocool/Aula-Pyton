f682c2a21d7c9998fe48f5eb62f89b192c910b98
Thiagocool <114118225+Thiagocool@users.noreply.github.com>
2022-10-12 18:25:32 -0300

    Add files via upload

---
+++ Lista.py
@@ -0,0 +1,15 @@
+# dado do tipo lista
+#dados multavel pode sofrer alteração
+
+
+
+Nome =input("Qual seu nome?:")
+sobrenome =input("qual seu sobrenome:")
+idade = input("Qual sua idade:")
+
+
+
+
+print("Bem vindo!")
+print(2*3+3**2)
+print(msg)
